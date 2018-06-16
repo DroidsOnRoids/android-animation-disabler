61db7de4095170d205918c22cef0c452229ee7e1
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-06-16 13:52:33 +0200

    Add device serial to info log when changing scale

--- plugin/src/main/kotlin/pl/droidsonroids/gradle/animation/AnimationDisablerPlugin.kt
+++ plugin/src/main/kotlin/pl/droidsonroids/gradle/animation/AnimationDisablerPlugin.kt
@@ -15,61 +15,62 @@ class AnimationDisablerPlugin : Plugin<Project> {
 
     private val androidSerials = System.getenv("ANDROID_SERIAL")?.split(',')
 
-	override fun apply(project: Project) {
-		project.pluginManager.apply(BasePlugin::class.java)
+    override fun apply(project: Project) {
+        project.pluginManager.apply(BasePlugin::class.java)
 
-		val androidPlugins = listOf("com.android.application", "com.android.library", "com.android.test")
-		androidPlugins.forEach {
-			project.plugins.withId(it) {
-				project.addAnimationTasksWithDependencies()
-			}
-		}
-	}
+        val androidPlugins = listOf("com.android.application", "com.android.library", "com.android.test")
+        androidPlugins.forEach {
+            project.plugins.withId(it) {
+                project.addAnimationTasksWithDependencies()
+            }
+        }
+    }
 
     private fun Project.addAnimationTasksWithDependencies() = afterEvaluate {
-		val disableAnimations = createAnimationScaleTask(false)
-		val enableAnimations = createAnimationScaleTask(true)
+        val disableAnimations = createAnimationScaleTask(false)
+        val enableAnimations = createAnimationScaleTask(true)
 
-		tasks.withType(DeviceProviderInstrumentTestTask::class.java).forEach {
-			it.dependsOn(disableAnimations)
-			it.finalizedBy(enableAnimations)
-		}
-	}
+        tasks.withType(DeviceProviderInstrumentTestTask::class.java).forEach {
+            it.dependsOn(disableAnimations)
+            it.finalizedBy(enableAnimations)
+        }
+    }
 
     private fun Project.createAnimationScaleTask(enableAnimations: Boolean): Task =
-			tasks.create("connected${if (enableAnimations) "En" else "Dis"}ableAnimations") {
-				val scale = if (enableAnimations) 1 else 0
+        tasks.create("connected${if (enableAnimations) "En" else "Dis"}ableAnimations") {
+            val scale = if (enableAnimations) 1 else 0
 
-				AndroidDebugBridge.initIfNeeded(false)
-				val android = project.extensions.getByType(BaseExtension::class.java)
-				val bridge = AndroidDebugBridge.createBridge(android.adbExecutable.path, false)
-				val shellOuptutReceiver = ADBShellOutputReceiver(it.logger)
+            AndroidDebugBridge.initIfNeeded(false)
+            val android = project.extensions.getByType(BaseExtension::class.java)
+            val bridge = AndroidDebugBridge.createBridge(android.adbExecutable.path, false)
+            val shellOuptutReceiver = ADBShellOutputReceiver(it.logger)
 
-				it.group = "verification"
-				it.description = "Sets animation scale to $scale on all connected Android devices and AVDs"
-				it.doLast {
-					bridge.setAnimationScale(scale, shellOuptutReceiver)
-				}
-			}
+            it.group = "verification"
+            it.description = "Sets animation scale to $scale on all connected Android devices and AVDs"
+            it.doLast {
+                bridge.setAnimationScale(scale, shellOuptutReceiver, project)
+            }
+        }
 
-    private fun AndroidDebugBridge.setAnimationScale(value: Int, shellOuptutReceiver: ADBShellOutputReceiver) {
-		val settingsPrefixes = listOf("window_animation", "transition_animation", "animator_duration")
-		var devicesToSet = devices
+    private fun AndroidDebugBridge.setAnimationScale(value: Int, shellOuptutReceiver: ADBShellOutputReceiver, project: Project) {
+        val settingsPrefixes = listOf("window_animation", "transition_animation", "animator_duration")
+        var devicesToSet = devices
 
-		if (androidSerials != null) {
-			devicesToSet = devices.filter {
-				it.serialNumber in androidSerials
-			}.toTypedArray()
-		}
+        if (androidSerials != null) {
+            devicesToSet = devices.filter {
+                it.serialNumber in androidSerials
+            }.toTypedArray()
+        }
 
-		devicesToSet.forEach { device ->
-			settingsPrefixes.forEach { prefix ->
-				device.setScaleSetting("${prefix}_scale", value, shellOuptutReceiver)
-			}
-		}
-	}
+        devicesToSet.forEach { device ->
+            settingsPrefixes.forEach { prefix ->
+                project.logger.info("Setting ${prefix}_scale to $value on ${device.serialNumber}")
+                device.setScaleSetting("${prefix}_scale", value, shellOuptutReceiver)
+            }
+        }
+    }
 
     private fun IDevice.setScaleSetting(key: String, value: Int, shellOuptutReceiver: ADBShellOutputReceiver) {
-		executeShellCommand("settings put global $key $value", shellOuptutReceiver, DdmPreferences.getTimeOut().toLong(), TimeUnit.MILLISECONDS)
-	}
+        executeShellCommand("settings put global $key $value", shellOuptutReceiver, DdmPreferences.getTimeOut().toLong(), TimeUnit.MILLISECONDS)
+    }
 }
